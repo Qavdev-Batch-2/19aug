aa6360cb8ef4da7dbbeb988e5ca8052c184368f5
Patrick Aisida <nagato234@gmail.com>
2024-08-29 11:28:50 +0100

    git commit -a

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,7 @@ module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
     requestTimeout: 6000,
-    defaultCommandTimeout: 8000,
-
-
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
