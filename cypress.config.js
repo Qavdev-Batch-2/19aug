3a53d5573a392746f94fd74c3c85589e080b264c
Patrick Aisida <nagato234@gmail.com>
2024-08-29 12:03:56 +0100

    Revert "git commit -a"
    This reverts commit aa6360cb8ef4da7dbbeb988e5ca8052c184368f5.
    
    gi

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,9 @@ module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
     requestTimeout: 6000,
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 8000,
+
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
