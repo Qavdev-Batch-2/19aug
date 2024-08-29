c94a70b9c37f19dffbad4f7bb19b08b3f6349093
Patrick Aisida <nagato234@gmail.com>
2024-08-29 13:24:50 +0100

    New commit

---
+++ cypress/e2e/loginpatrick.cy.js
@@ -0,0 +1,15 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+  });
+
+describe('Testing Login', () => {
+  it('Should be Login', () => {
+    cy.visit('https://www.konga.com/')
+    cy.get('._12e27_1r3kc > ._7ad32_SD12Y').click()
+    cy.get('#username').type('yahiko56@yopmail.com')
+    cy.get('#password').type('Kisame53#', {log: false})
+    cy.get('.eed68_3IZCC > ._2aac2_3bwnD > ._0a08a_3czMG').click()
+  })
+})
